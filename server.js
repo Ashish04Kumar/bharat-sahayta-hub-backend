e9898adedb92b879f8b2b8c10fefe47350fef2b5
Ashish Kumar <ashish.kumar@bombaysoftwares.com>
2025-10-22 14:35:43 +0530

    fix: logout process

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import translationRoutes from "./routes/translationRoutes.js";
-import { loginUser, logoutUser, registerUserController } from "./controllers/resgistrationUserController.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUserController,
+} from "./controllers/resgistrationUserController.js";
 import bodyParser from "body-parser";
 import upload from "./utils/multer.js";
-
 dotenv.config();
+
 connectDB();
 
 const app = express();
@@ -30,9 +34,15 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1", translationRoutes);
-app.post("/api/v1/register-user", upload.single("profilePicture"), registerUserController);
+app.post(
+  "/api/v1/register-user",
+  upload.single("profilePicture"),
+  registerUserController
+);
 app.post("/api/v1/login-user", loginUser);
 app.post("/api/v1/logout-user", logoutUser);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server running on 0.0.0.0:${PORT}`)
+);
